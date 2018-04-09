0823e15169f028b42b954b474e8092512ec6552d
xinjie <38008973+vfp9@users.noreply.github.com>
2018-04-09 13:27:04 +0800

    Add files via upload

---
+++ Thor/Thor/Tools/Procs/thor_tool_procs.h
@@ -0,0 +1,17 @@
+#IF Version(3) = [00]		&& English
+	#Define _Manage_Plug_in_PRGs		[Manage Plug in PRGs]
+	#Define _Plug_ins_Home_Page		[Plug ins Home Page]
+	#Define _Plug_In					[Plug In]
+	#Define _Action					[Action]
+	#Define _Description				[Description]
+	#Define _Applies_to				[Applies to]
+
+#ELSE Version(3) = [86]		&& Simplified Chinese
+	#Define _Manage_Plug_in_PRGs		[插件管理器]
+	#Define _Plug_ins_Home_Page		[插件主页]
+	#Define _Plug_In					[插件]
+	#Define _Action					[动作]
+	#Define _Description				[描述]
+	#Define _Applies_to				[应用于]
+
+#ENDIF
